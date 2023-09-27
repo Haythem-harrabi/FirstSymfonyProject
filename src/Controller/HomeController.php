c0ef69234ba310d368c0129a3f297ebbad1497ce
haythem harrabi <harrabihaithem21@gmail.com>
2023-09-27 14:05:58 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    //Methode qui permet d'afficher Bonjour mes étudiants
+    #[Route('/show', name: 'app_show')]
+    public function show(): Response
+    {
+        return new Response('<h1> Bonjour mes étudiants </h1>');
+    }
+}
---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service', name: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+
+
+
+    //Methode showService()
+    #[Route('/service/show/{name}', name: 'app_show_service')]
+    public function showService($name): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+
+
+    //Methode de la redirection vers la page "Bonjour mes etudiants"
+    #[Route('/service/GoToIndex', name: 'app_GoToIndex')]
+    public function goToIndex()
+    {
+        
+        return $this->redirectToRoute('app_show');
+    }
+}
